65c13f08f798c9b6f15a96022e14a6bea4484877
Tomas Gonzalez <tomasgp14@hotmail.com>
2022-10-19 00:26:39 -0400

    adding player logic

--- 3d-components/fighter/hooks/useLogic.ts
+++ 3d-components/fighter/hooks/useLogic.ts
@@ -1,43 +1,46 @@
 import { useRef } from 'react';
 import useMainStore from '../../../store/mainStore';
 import { MeshBasicMaterialProps, useFrame } from '@react-three/fiber';
-import { ArrowHelper, Raycaster, Vector3 } from 'three';
+import { Raycaster, Vector3 } from 'three';
 
 const SPEED = 0.5;
+const STOP_DISTANCE = 0.2;
 
-const handleMovement = () => {};
+const calculateMovement = (intersects, delta, left) => {
+  if (left) {
+    return 0; //temporary behavior
+  }
+
+  if (intersects[0]?.distance > STOP_DISTANCE) {
+    return left ? 0 : delta * -SPEED;
+  }
+  return 0;
+};
 
 const useLogic = (left) => {
   const hitboxRef = useRef<MeshBasicMaterialProps>();
   const handGesture = useMainStore((store) => store.handGesture);
   const raycaster = new Raycaster();
-  const inRange = useRef(false);
-  let arrow = new ArrowHelper(
-    raycaster.ray.direction,
-    raycaster.ray.origin,
-    1,
-    Math.random() * 0xffffff
-  );
 
   useFrame((_state, delta) => {
     raycaster.set(hitboxRef.current.position, new Vector3(left ? 1 : -1, 0, 0));
-    _state.scene.remove(arrow);
-    arrow = new ArrowHelper(
-      raycaster.ray.direction,
-      raycaster.ray.origin,
-      1,
-      Math.random() * 0xffffff
-    );
 
-    _state.scene.add(arrow);
-
+    const intersects = raycaster.intersectObjects(_state.scene.children);
     switch (handGesture[0]) {
       case 2:
-        hitboxRef.current.position.x += left ? 0 : delta * -SPEED;
+        hitboxRef.current.position.x += calculateMovement(
+          intersects,
+          delta,
+          left
+        );
         break;
 
       case 1:
-        hitboxRef.current.position.x += left ? 0 : delta * SPEED;
+        hitboxRef.current.position.x += calculateMovement(
+          intersects,
+          delta,
+          left
+        );
         break;
 
       default:
