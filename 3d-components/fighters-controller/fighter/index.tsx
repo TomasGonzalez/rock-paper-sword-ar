20a1f942b214a66227ce855c565a11c42d3f8679
Tomas Gonzalez <tomasgp14@hotmail.com>
2022-10-25 14:28:29 -0400

    implementing xState state machine

--- 3d-components/fighters-controller/fighter/index.tsx
+++ 3d-components/fighters-controller/fighter/index.tsx
@@ -1,6 +1,75 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { Raycaster, Vector3 } from 'three';
+import { Machine } from 'xstate';
+import useMainStore from '../../../store/mainStore';
 
-const Fighter = (props, ref) => {
+const SPEED = 0.5;
+const STOP_DISTANCE = 0.2;
+
+const createMachine = (props) => {
+  const machine = Machine({
+    id: props.key,
+    initial: 'idle',
+    states: {
+      idle: {
+        on: {
+          WALK: 'walk',
+        },
+      },
+      walk: {},
+      shield: {},
+      kick: {},
+      sword: {},
+      fall: {},
+    },
+    on: {
+      CHANGE: [
+        {
+          target: 'idle',
+          actions: (_, e) => {
+            console.log("I'm idle");
+          },
+        },
+      ],
+    },
+  });
+  return machine;
+};
+
+const Fighter = (props) => {
+  const handGesture = useMainStore((store) => store.handGesture);
+  const raycaster = useRef<any>(new Raycaster());
+  const machine = useRef(createMachine(props));
+  const ref = useRef<any>();
+
+  const calculateMovement = (state, delta) => {
+    if (props.left) {
+      return 0; //temporary behavior
+    }
+
+    const intersects = raycaster?.current.intersectObjects(
+      state.scene.children
+    );
+
+    console.log(intersects[0], 'this are intersects');
+    if (intersects[0]?.distance < 0.4) {
+      return 0;
+    }
+
+    return props.left ? 0 : delta * -SPEED;
+  };
+
+  useFrame((state, delta) => {
+    if (raycaster.current && ref.current) {
+      raycaster.current.set(
+        ref.current.position,
+        new Vector3(props.left ? 1 : -1, 0, 0)
+      );
+      ref.current.position.x += calculateMovement(state, delta);
+    }
+  });
+
   return (
     <mesh {...props} ref={ref}>
       <boxGeometry args={[1, 1, 1]} />
--- 3d-components/fighters-controller/index.tsx
+++ 3d-components/fighters-controller/index.tsx
@@ -1,27 +1,11 @@
 import { memo, useRef } from 'react';
-import useMainStore from '../../store/mainStore';
-import { useFrame } from '@react-three/fiber';
-import { Raycaster, Vector3, ArrowHelper } from 'three';
 import Fighter from './fighter';
-
-const SPEED = 0.5;
-const STOP_DISTANCE = 0.2;
-
-enum actions {
-  'stop',
-  'walk',
-  'attack',
-  'fall',
-}
 
 interface FighterType {
   key: string;
   position: number[];
   scale: number[];
-  raycaster: Raycaster;
-  cooldownThreshold: number;
-  action: number;
-  cooldown: number;
+  left?: boolean;
 }
 
 const fightersList: FighterType[] = [
@@ -29,85 +13,20 @@ const fightersList: FighterType[] = [
     key: 'fighter1',
     position: [1, 0.06, 0],
     scale: [0.2, 0.4, 0.2],
-    cooldown: 0, //current cooldown
-    cooldownThreshold: 1, // cooldown threshold for the action
-    action: 0, // 0 attacking 1 walking
-    raycaster: new Raycaster(),
   },
   {
     key: 'fighter2',
     position: [-1, 0.06, 0],
     scale: [0.2, 0.4, 0.2],
-    cooldown: 0,
-    cooldownThreshold: 1,
-    action: 0,
-    raycaster: new Raycaster(),
+    left: true,
   },
 ];
 
-const viewRaycast = (_state, index) => {
-  _state.scene.add(
-    new ArrowHelper(
-      fightersList[index].raycaster.ray.direction,
-      fightersList[index].raycaster.ray.origin,
-      0.5,
-      0xff0000
-    )
-  );
-};
-
-const calculateMovement = (raycaster, delta, left, _state, action) => {
-  if (left) {
-    return 0; //temporary behavior
-  }
-
-  const intersects = raycaster?.intersectObjects(_state.scene.children);
-
-  if (action === actions.walk && intersects[0]?.distance > STOP_DISTANCE) {
-    return left ? 0 : delta * -SPEED;
-  }
-
-  return 0;
-};
-
-const actionSelection = (handGesture, fighterRef) => {
-  return actions.stop;
-};
-
 function FightersController() {
-  const handGesture = useMainStore((store) => store.handGesture);
-  const fightersRefs = useRef<any>([]);
-
-  useFrame((_state, delta) => {
-    if (fightersRefs.current.length) {
-      fightersRefs.current.forEach((fighterRef, index) => {
-        fightersList[index].raycaster.set(
-          fighterRef.position,
-          new Vector3(index ? 1 : -1, 0, 0)
-        );
-
-        fighterRef.action = actionSelection(handGesture, fighterRef);
-
-        fighterRef.position.x += calculateMovement(
-          fightersList[index].raycaster,
-          delta,
-          index,
-          _state,
-          fighterRef.action
-        );
-        // viewRaycast(_state, index);
-      });
-    }
-  });
-
   return (
     <>
       {fightersList.map((fightersData, index) => (
-        <Fighter
-          ref={(el) => (fightersRefs.current[index] = el)}
-          key={fightersData.key}
-          {...fightersData}
-        />
+        <Fighter key={fightersData.key} {...fightersData} />
       ))}
     </>
   );
